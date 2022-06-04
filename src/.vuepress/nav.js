13beca04f128026b219a8a8991137952adb36219
zhoubichuan <zhoubichuan@icloud.com>
2022-06-05 01:45:09 +0800

    master

---
+++ src/.vuepress/nav.js
@@ -0,0 +1,102 @@
+const nav = [
+    {
+        text: '基础知识',
+        items: [
+            {
+                text: '一.linux',
+                link: '/base/linux/1.index'
+            },
+            {
+                text: '二.CICD',
+                link: '/base/auth/1.index',
+            },
+            {
+                text: '七.system',
+                link: '/base/system/1.index',
+            },
+            {
+                text: '八.service',
+                link: '/base/service/1.index',
+            },
+            {
+                text: '九.network',
+                link: '/base/network/1.index',
+            }
+        ]
+    },
+    {
+        text: '相关应用',
+        items: [
+            {
+                text: '一.vi',
+                link: '/application/vi/1.index',
+            },
+            {
+                text: '二.Kubernetes',
+                link: '/application/user/1.index',
+            },
+            {
+                text: '三.VMware',
+                link: '/application/shell/1.index',
+            },
+            {
+                text: '四.Parallels',
+                link: '/application/install/1.index',
+            },
+        ]
+    },
+    {
+        text: '高级知识',
+        items: [
+            {
+                text: '一.nginx',
+                link: '/senior/nginx/1.index'
+            },
+            {
+                text: '二.docker',
+                link: '/senior/docker/1.index'
+            },
+            {
+                text: '三.jenkins',
+                link: '/senior/jenkins/1.index'
+            },
+            {
+                text: '四.groovy',
+                link: '/senior/groovy/1.index'
+            },
+            {
+                text: '五.php',
+                link: '/senior/php/1.index'
+            },
+            {
+                text: '六.java',
+                link: '/senior/java/1.index'
+            },
+            {
+                text: '七.node',
+                link: '/senior/node/1.index'
+            },
+            {
+                text: '八.k8s',
+                link: '/senior/k8s/1.index'
+            },
+        ]
+    },
+    // {
+    //   text: '源码知识',
+    //   items: [{
+    //       text: '1.vue源码解析',
+    //       link: '/source/vue/1.preparation'
+    //     },
+    //     {
+    //       text: '2.vuex源码解析',
+    //       link: '/source/vuex'
+    //     },
+    //     {
+    //       text: '3.vue-router源码解析',
+    //       link: '/source/vue-router'
+    //     },
+    //   ]
+    // }
+]
+module.exports = nav
--- src/README.md
+++ src/README.md
@@ -1,7 +1,7 @@
 ---
 lang: zh-CN
 home: true
-heroImage: ""
+heroImage: ./home.png
 actionText: 先睹为快 →
 actionLink: /base/build/1.config
 pageClass: page_index
@@ -16,28 +16,16 @@ meta:
         <div id="large-header" class="large-header"></div>
             <div class="features">
         <div class="feature">
-          <h2>devops项目的工程化</h2> 
+          <h2>基础知识</h2> 
           <p>掌握devops组件间的多种通信方式及数据同步 渲染函数及jsx高阶应用 devops-cli3、devopsx、devops-router</p>
         </div>
         <div class="feature">
-          <h2>devops项目功能模块</h2> 
+          <h2>相关应用</h2> 
           <p>掌握一般项目中的配置、登录、权限认证、单元测试、国际化、富文本、上传下载等常见功能</p>
         </div>
         <div class="feature">
-          <h2>devops组件的开发</h2> 
+          <h2>高级知识</h2> 
           <p>组件设计思路，组件编写工作流搭建 从0编写复杂组件之异步级联组件 单元测试编写及组件的发布</p>
-        </div>
-        <div class="feature">
-          <h2>devops优化和服务器布署</h2> 
-          <p>devops优化预渲染、骨架屏、Nuxt.js服务端渲染 使用typescript构建devops应用 devops + nginx实现devops的布署和持续集成</p>
-        </div>
-        <div class="feature">
-          <h2>devops2.0源码实现</h2> 
-          <p>devops/cli原理、devops2.0原理剖析、devops-router原理剖析、devopsx原理剖析 </p>
-        </div>
-        <div class="feature">
-          <h2>devops3.0源码实现</h2> 
-          <p>devops/cli原理、devops3.0原理剖析、devops-router、devopsx源码实现</p>
         </div>
       </div>
     </div>
