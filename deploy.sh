d75ee26d99bc80219d2fe5856585d79b4ae74e02
zhoubichuan <zhoubichuan@icloud.com>
2022-06-04 23:48:55 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ set -e
 git init
 git add -A
 git commit -m 'master'
-git pull origin master
-git push -u origin master
-git push origin master
+git pull github master
+git push -u github master
+git push github master
 
 # 2.打包
 npm run src:build
@@ -22,8 +22,33 @@ git init
 git checkout --orphan gh-pages
 git add .
 git commit -m 'gh-pages'
-git remote add origin https://github.com/zhoubichuan/Web-devops.git
-git push -f origin gh-pages
+git remote add github https://github.com/zhoubichuan/web-devops.git
+git push -f github gh-pages
+
+# 5.返回初始目录
+cd -
+
+# 1.发布到master分支
+git init
+git add -A
+git commit -m 'master'
+git pull gitee master
+git push -u gitee master
+git push gitee master
+
+# 2.打包
+npm run src:build
+
+# 3.进入打包目录
+cd dist
+
+# 4.发布到gh-pages分支
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m 'gh-pages'
+git remote add gitee https://github.com/zhoubichuan/web-devops.git
+git push -f gitee gh-pages
 
 # 5.返回初始目录
 cd -
--- github.sh
+++ github.sh
@@ -11,7 +11,7 @@ git init
 git checkout --orphan gh-pages
 git add .
 git commit -m 'gh-pages'
-git remote add origin https://github.com/zhoubichuan/Web-devops.git
+git remote add origin https://github.com/zhoubichuan/web-devops.git
 git push -f origin gh-pages
 
 # 4.返回初始目录
